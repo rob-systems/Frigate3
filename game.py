d8b52d4914420979ac1d11f4c129fa0816c1d725
rob-systems <rob.thomas.epsom@gmail.com>
2023-02-12 12:14:13 +0000

    Basic Menu
    Added menu file  which displays as soon as window opens. When Play Game is pressed menu closes and game initialization can begin. In game.py created menu class and call the loop function

--- game.py
+++ game.py
@@ -1,19 +1,45 @@
+# Welcome to Frigate
 import pygame, sys
+from menu import Menu
 from pygame.locals import *
 
+
+#Allows pygame functions to be called
 pygame.init()
 
+# Declare Width and Height of Display
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 
-displaysurf = pygame.display.set_mode((800, 600))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+# Function handling Menu and Game
+def main():
+    # Create Display Surface
+    displaysurf = pygame.display.set_mode((800, 600))
+    play_game_pressed = False
+    menu = Menu()
+    while True:
+        ############ Quit Event Handling ###########
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        ############################################
+
+        if not play_game_pressed:
+            play_game_pressed = menu.loop(displaysurf, pygame.mouse.get_pos())
+            
+
+        else:
+            displaysurf.fill((0,0,0))
+        
+        # internally process pygame event handlers
+        pygame.event.pump()
+        # Update portions of the screen for software displays
+        pygame.display.update()
 
 
-    
-    pygame.event.pump()
-    pygame.display.update()
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
